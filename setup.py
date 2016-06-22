841f0cea6b3fab4e34f9a511d6c8868019715fe4
Jari <jtorniainen@users.noreply.github.com>
2016-06-22 12:47:44 +0300

    tweaking the modularization

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='whatis',
-      version='0.0.1',
+      version='0.1',
       description='A diagnostic tool for inspecting variables',
       license='MIT',
-      scripts=['whatis'],
+      packages=['whatis'],
       include_package_data=False)
